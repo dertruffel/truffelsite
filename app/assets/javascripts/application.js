e2a119ad7d22f9f0be477bbb908db22ffd998f1e
dertruffel <tomsonrz@protonmail.ch>
2021-10-27 21:29:55 +0200

    file update

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require will_paginate_infinite
+
+//= require jquery
+
+//= require bootstrap-sprockets
+//= require bootstrap
+
+//= require_tree
---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+
+#Set environment before loading the app ATH
+
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+
+
